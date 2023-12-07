8729ab6bb1753e2b960a4f3a15dd80bcf2ec17ab
Bruno Capuano <bcapuano@gmail.com>
2023-12-07 11:03:20 -0500

    drone agend and drone research in progress

--- dotnet/samples/07-DroneAssistant/Program.cs
+++ dotnet/samples/07-DroneAssistant/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using Microsoft.SemanticKernel.AI.ChatCompletion;
 using Microsoft.SemanticKernel.Handlebars;
+using System.Runtime.CompilerServices;
 
 string OpenAIApiKey = Env.Var("OpenAI:ApiKey")!;
 string BingApiKey = Env.Var("Bing:ApiKey")!;
@@ -18,50 +19,84 @@ IPlugin mathPlugin = new Plugin(
 
 Plugin ollamaGenerationPlugin = new Plugin(
     name: "Math",
-    functions: new () {
+    functions: new() {
         SemanticFunction.GetFunctionFromYaml(currentDirectory + "/Plugins/Ollama/Math.prompt.yaml")
     }
 );
 
-IPlugin searchPlugin = new Plugin(
-    name: "Search",
-    functions: NativeFunction.GetFunctionsFromObject(new Search(BingApiKey))
-);
 
-// Create a researcher
-IPlugin researcher = AssistantKernel.FromConfiguration(
-    currentDirectory + "/Assistants/Researcher.agent.yaml",
-    aiServices: new () { gpt35Turbo, },
-    plugins: new () { searchPlugin }
-);
+//IPlugin searchPlugin = new Plugin(
+//    name: "Search",
+//    functions: NativeFunction.GetFunctionsFromObject(new Search(BingApiKey))
+//);
+
+//// Create a researcher
+//IPlugin researcher = AssistantKernel.FromConfiguration(
+//    currentDirectory + "/Assistants/Researcher.agent.yaml",
+//    aiServices: new () { gpt35Turbo, },
+//    plugins: new () { searchPlugin }
+//);
 
 // Create a mathmatician
 IPlugin mathmatician = AssistantKernel.FromConfiguration(
     currentDirectory + "/Assistants/Mathmatician.agent.yaml",
-    aiServices: new () { gpt35Turbo, ollamaGeneration },
-    plugins: new () { mathPlugin }
+    aiServices: new() { gpt35Turbo, ollamaGeneration },
+    plugins: new() { mathPlugin }
 );
+
+Plugin ollamaGenerationPluginDrone = new Plugin(
+    name: "Drone",
+    functions: new() {
+        SemanticFunction.GetFunctionFromYaml(currentDirectory + "/Plugins/TelloDrone/TelloDrone.prompt.yaml")
+    }
+);
+
+// Create a mathmatician
+IPlugin droneCodeGen = AssistantKernel.FromConfiguration(
+    currentDirectory + "/Assistants/DroneCodeGenerator.agent.yaml",
+    aiServices: new() { gpt35Turbo, ollamaGeneration },
+    plugins: new() { ollamaGenerationPluginDrone }
+);
+
 
 // Create a Project Manager
 AssistantKernel projectManager = AssistantKernel.FromConfiguration(
     currentDirectory + "/Assistants/ProjectManager.agent.yaml",
-    aiServices: new () { gpt35Turbo },
-    plugins: new () { researcher, mathmatician }
+    aiServices: new() { gpt35Turbo },
+    plugins: new() { mathmatician, droneCodeGen }
 );
 
 IThread thread = await projectManager.CreateThreadAsync();
-while(true) {
+bool keepRunning = true;
+while (keepRunning)
+{
     // Get user input
     Console.Write("User > ");
-    thread.AddUserMessageAsync(Console.ReadLine());
-
-    // Run the thread using the project manager kernel
-    var result = await projectManager.RunAsync(thread);
-
-    // Print the results
-    var messages = result.GetValue<List<ModelMessage>>();
-    foreach(ModelMessage message in messages)
+    string userInput = Console.ReadLine();
+    if (userInput != null)
     {
-        Console.WriteLine("Project Manager > " + message);
+        // validate if the user wants to exit
+        if (userInput.ToLower() == "exit")
+        {
+            keepRunning = false;
+            continue;
+        }
+
+        // validate if uyser ipunt is "d"
+        if (userInput.ToLower() == "d")
+            userInput = "generate a flight plan for a drone with the following actions: takeoff the drone, move forward 25 centimeters, flip right, move down 30 centimeters and land";
+
+
+        _ = thread.AddUserMessageAsync(userInput);
+
+        // Run the thread using the project manager kernel
+        var result = await projectManager.RunAsync(thread);
+
+        // Print the results
+        var messages = result.GetValue<List<ModelMessage>>();
+        foreach (ModelMessage message in messages)
+        {
+            Console.WriteLine("Project Manager > " + message);
+        }
     }
 }
